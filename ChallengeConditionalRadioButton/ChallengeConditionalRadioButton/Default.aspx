277d0caed618cdb1ef98021b658358be22b460ff
starfish01 <patrick.labes@gmail.com>
2017-10-14 15:10:22 +1000

    Basic Challenge involved radiobuttons and images. There  has to be a better way of looking at radiobuttons in a group but i couldnt find it

---
+++ ChallengeConditionalRadioButton/ChallengeConditionalRadioButton/Default.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeConditionalRadioButton.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Your note taking preference</div>
+        <br />
+        <asp:RadioButton ID="radioPencil" runat="server" Checked="True" GroupName="noteGroup" Text="Pencil" />
+        <br />
+        <asp:RadioButton ID="radioPen" runat="server" GroupName="noteGroup" Text="Pen" />
+        <br />
+        <asp:RadioButton ID="radioPhone" runat="server" GroupName="noteGroup" Text="Phone" />
+        <br />
+        <asp:RadioButton ID="radioTablet" runat="server" GroupName="noteGroup" Text="Tablet" />
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        <br />
+        <br />
+        <asp:Image ID="imageResult" runat="server" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ ChallengeConditionalRadioButton/ChallengeConditionalRadioButton/Default.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeConditionalRadioButton
+{
+    public partial class Default : System.Web.UI.Page
+    {
+
+        private String resultLabelText = "You have selected ";
+        private RadioButton radioBtnChecked;
+        
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            whatRadioButtonIsSelected();
+            
+        }
+
+        private void whatRadioButtonIsSelected()
+        {
+           
+            if (radioPen.Checked)
+            {
+                resultLabel.Text = resultLabelText + "Pen.";
+                imageResult.ImageUrl = "pen.png";
+            }
+            else if (radioPencil.Checked)
+            {
+                resultLabel.Text = resultLabelText + "Pencil.";
+                imageResult.ImageUrl = "pencil.png";
+
+            }
+            else if (radioPhone.Checked)
+            {
+                imageResult.ImageUrl = "phone.png";
+                resultLabel.Text = resultLabelText + "Phone.";
+            }
+            else if (radioTablet.Checked)
+            {
+                imageResult.ImageUrl = "tablet.png";
+                resultLabel.Text = resultLabelText + "Tablet.";
+            }
+
+        }
+
+       
+
+    }
+}
