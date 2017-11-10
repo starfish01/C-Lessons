d3786fb4cd403e01562d062b23429ae216b41b1c
starfish01 <patrick.labes@gmail.com>
2017-11-11 08:56:28 +1000

    11/11 Havent added much not sure how the challenge wants to use the filds damageMax and attackBonus

---
+++ ChallengeHeroMonsterClasses/ChallengeHeroMonsterClasses/Character.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChallengeHeroMonsterClasses
+{
+    public class Character
+    {
+        Random rnd = new Random();
+
+        public String Name { get; set; }
+        public Double Health {
+            get
+            {
+                return Health;
+            }
+            set
+            {
+                Health = 100;
+            }
+
+        }
+        public int DamageMaximum { get; set; }
+        
+        public int AttackBonus { get; set; }
+       
+    }
+}
