5ab828533fed4d82b458f6dd77080f3f4c3378c3
starfish01 <patrick.labes@gmail.com>
2017-11-13 17:27:17 +1000

    set up part two of the challenge using a dice class

---
+++ ChallengeHeroMonsterClasses/ChallengeHeroMonsterClasses/Dice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChallengeHeroMonsterClasses
+{
+    public class Dice
+    {
+
+        public int Roll()
+        {
+
+
+
+            return 0;
+        }
+
+
+    }
+}
